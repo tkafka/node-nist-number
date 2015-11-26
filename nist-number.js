d20fed8ebb13f81e603b905daa61bc47ce755f14
Tomas Kafka <tk@tomaskafka.com>
2015-11-26 10:00:37 +0100

    test passes

--- nist-number.js
+++ nist-number.js
@@ -1,12 +1,14 @@
 module.exports = function(n) {
+	if (typeof n != 'number') return null;
 	var s = n.toString();
 
 	// get stuff before the dot
 	var d = s.indexOf('.');
 	var s2 = d === -1 ? s : s.slice(0, d);
+	var negative = n < 0;
 
 	// insert spaces every 3 digits from the right
-	for (var i = s2.length - 3; i > 0; i -= 3)
+	for (var i = s2.length - 3; i > negative ? 1 : 0; i -= 3)
 		s2 = s2.slice(0, i) + ' ' + s2.slice(i);
 
 	// append fractional part
